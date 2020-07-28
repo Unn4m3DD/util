a152dad6b0ce4744cf9e5552f0ca6dcddf624280
André Patacas <guerreiroapatacas@gmail.com>
2020-07-28 16:41:52 +0100

    vscode config script created

---
+++ vscode/make-me-a-sandwich.sh
@@ -0,0 +1,25 @@
+sudo pacman -Sy ttf-fira-code
+cp keybindings.json /home/$(whoami)/.config/Code/User/keybindings.json 
+cp settings.json /home/$(whoami)/.config/Code/User/settings.json
+code --install-extension ms-vscode-remote.remote-ssh
+cp ssh /home/$(whoami)/.ssh/config
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension oderwat.indent-rainbow
+code --install-extension ms-vscode.cpptools
+code --install-extension dracula-theme.theme-dracula
+code --install-extension eamodio.gitlens
+code --install-extension pkief.material-icon-theme
+#code --install-extension 
+#code --install-extension 
+#code --install-extension 
+#code --install-extension 
+
+
+
+
+
+
+
+
+
+
