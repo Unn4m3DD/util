c823fc0f9574303a83d1904e618d2d60c3f1c342
Unn4m3DD <guerreiroapatacas>
2021-01-16 10:14:37 +0000

    hardlink migration

--- desktop_env/install.sh
+++ desktop_env/install.sh
@@ -1,4 +1,8 @@
-cp -r i3.d/i3 ~/.config
-cp -r i3.d/i3status-rust ~/.config
+mkdir ~/.config/i3/
+mkdir ~/.config/i3status-rust/
+
+ln i3.d/i3/config ~/.config/i3/config
+ln i3.d/i3status-rust/config.toml ~/.config/i3status-rust/config.toml
+
 printf "#!/bin/sh\nfeh --no-fehbg --bg-scale '/home/andre/util/Final.png'" > ~/.fehbg
 chmod +x ~/.fehbg
