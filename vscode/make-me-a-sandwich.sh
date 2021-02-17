d676e51da87bd6429adecd7d9043d6b0f26b9e52
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-02-17 17:10:33 +0000

    ln added to vscode config

--- vscode/make-me-a-sandwich.sh
+++ vscode/make-me-a-sandwich.sh
@@ -1,8 +1,13 @@
 sudo pacman -Sy ttf-fira-code
-cp keybindings.json /home/$(whoami)/.config/Code/User/keybindings.json 
-cp settings.json /home/$(whoami)/.config/Code/User/settings.json
+
+rm -f /home/$(whoami)/.config/Code/User/keybindings.json 
+rm -f /home/$(whoami)/.config/Code/User/settings.json
+rm -f /home/$(whoami)/.ssh/config
+
+ln keybindings.json /home/$(whoami)/.config/Code/User/keybindings.json 
+ln settings.json /home/$(whoami)/.config/Code/User/settings.json
 code --install-extension ms-vscode-remote.remote-ssh
-cp ssh /home/$(whoami)/.ssh/config
+ln ssh /home/$(whoami)/.ssh/config
 code --install-extension coenraads.bracket-pair-colorizer-2
 code --install-extension oderwat.indent-rainbow
 code --install-extension ms-vscode.cpptools
@@ -13,8 +18,6 @@ code --install-extension coolbear.systemd-unit-file
 code --install-extension dpkristensen-garmin.udev-rules
 code --install-extension streetsidesoftware.code-spell-checker 
 #code --install-extension 
-
-
 
 
 
