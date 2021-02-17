bbdaae23cbff81cdaaf76dbf382a6b18b41b9f51
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-02-17 17:19:32 +0000

    i3 config changed to ln

--- desktop_env/install.sh
+++ desktop_env/install.sh
@@ -1,6 +1,9 @@
 mkdir ~/.config/i3/
 mkdir ~/.config/i3status-rust/
 
+rm -f ~/.config/i3/config
+rm -f ~/.config/i3status-rust/config.toml
+
 ln i3.d/i3/config ~/.config/i3/config
 ln i3.d/i3status-rust/config.toml ~/.config/i3status-rust/config.toml
 
