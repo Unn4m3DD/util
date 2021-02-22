6737d225b946fea3a05d5c7400097421a1d07cc9
André <guerreiroapatacas@gmail.com>
2021-02-22 09:52:59 +0000

    vscode install automated

--- vscode/make-me-a-sandwich.sh
+++ vscode/make-me-a-sandwich.sh
@@ -1,6 +1,7 @@
 current_dir=$(pwd)
 sudo pacman -Sy ttf-fira-code --noconfirm
 mkdir ~/AUR/
+cd ~/AUR/
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd visual-studio-code-bin
 makepkg -si --noconfirm
